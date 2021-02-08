53ea48c89b1c0421d0c6b93bcf853e6ab3bb478a
kwtuku <ttyttyttitti@gmail.com>
2021-02-08 16:36:56 +0900

    titleカラムとbodyカラムを作成

--- db/migrate/20210207195538_create_questions.rb
+++ db/migrate/20210207195538_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.text :body
       t.timestamps
     end
   end
