90ab74c0ca4748e59971a58cbcd64f1a9432ed4d
kwtuku <ttyttyttitti@gmail.com>
2021-02-08 18:20:56 +0900

    createアクションを追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,20 @@ class QuestionsController < ApplicationController
   def new
     @new = Question.new
   end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice] = "成功！"
+      redirect_to("/questions/new")
+    else
+      flash.now[:alert] = "失敗！"
+      render("questions/new")
+    end
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/questions", to: "questions#index"
   get "/questions/new", to: "questions#new"
+  post "/questions", to: "questions#create"
 end
