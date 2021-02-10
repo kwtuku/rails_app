2dee4fa087563885b408c3ae523fd5013a21dcfb
kwtuku <ttyttyttitti@gmail.com>
2021-02-11 01:54:04 +0900

    ユーザーのshowアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "questions#index"
   get "/questions", to: "questions#index"
