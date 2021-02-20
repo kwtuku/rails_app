ebdab2c99f646b1b2952a4ac44774d7b4d41a09e
kwtuku <ttyttyttitti@gmail.com>
2021-02-20 23:34:56 +0900

    URLを変更したからアクションも変更

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,10 +4,12 @@ class AnswersController < ApplicationController
   def create
     @answer = Answer.new(answer_params)
     @answer.user_id = current_user.id
+    @answer.question_id = params[:question_id]
     if @answer.save
       flash[:notice] = "成功！"
-      redirect_to("/questions/???")
+      redirect_to("/questions/#{params[:question_id]}")
     else
+      @question = Question.find(params[:question_id])
       flash.now[:alert] = "失敗！"
       render "questions/show"
     end
